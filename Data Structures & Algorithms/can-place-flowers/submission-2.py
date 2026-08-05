bf047a2eaab9e977ca71036e8f62bc4b78bbfac8
mvmohitverma31 <mvmohitverma.31@gmail.com>
2026-08-05 22:46:14 +0530

    Add: can-place-flowers - submission-2

---
+++ Data Structures & Algorithms/can-place-flowers/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        left=0
+        current=1
+        right=2
+        while right<=len(flowerbed)-1:
+            if flowerbed[current]==0:
+                if flowerbed[left]==0 and flowerbed[right]==0:
+                    flowerbed[current]=1
+                    n-=1
+                else:
+                    left+=1
+                    current+=1
+                    right+=1
+            else:
+                left+=1
+                current+=1
+                right+=1
+        if n==0:
+            return True
+        else:
+            return False
